ae5762d6f8c213bd3325024c02a35234fcf2e342
JulienOza <julien.ozarowski@gmail.com>
2023-10-03 12:51:29 +0200

    creating function helloWilders

---
+++ index.js
@@ -0,0 +1,9 @@
+import chalk from "chalk";
+
+function helloWilders() {
+    console.log(chalk.bgBlueBright('Rayane'));
+        console.log(chalk.bgGreenBright('Florence'));
+        console.log(chalk.bgYellowBright('Lucas'));
+        console.log(chalk.bgMagentaBright('Farid'));
+}
+helloWilders();
